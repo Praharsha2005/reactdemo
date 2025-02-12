d897af94a3dbc3fa161f4edba52420f88fcdd218
PraharshaP <2211cs010462@mallareddyuniversity.ac.in>
2025-02-12 19:17:36 +0530

    todays code

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React from 'react'
+import Logo from './Logo'
+import Menu from './Menu'
+import Styles from "./navbar.module.css";
 
-class Navbar extends React.Component{
-    render(){
-        return(
-            <div className="navbar">
-                <h1>Navabr</h1>
-            </div>
-        )
-    }
+const Navbar = () => {
+  return (
+    <section id={Styles.navBlock}>
+        <article>
+            <Logo/>
+            <Menu/>
+        </article>
+    </section>
+  )
 }
 
-export default Navbar;
+export default Navbar
--- src/propexample/SubChildProps.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-const SubChildProps = () => {
-    return(
-        <div>SubChildProps</div>
-    )
-}
-export default SubChildProps
